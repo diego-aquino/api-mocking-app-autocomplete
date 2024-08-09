db7c9ed42e19ab20b55679ad0dd1c1176cab9cf0
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-08-08 22:47:34 -0300

    Initial commit

---
+++ tests/autocomplete.test.ts
@@ -0,0 +1,45 @@
+import supertest from 'supertest';
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  test,
+} from 'vitest';
+
+import app from '../src/server/app';
+
+describe('Autocomplete', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  beforeEach(async () => {});
+
+  afterEach(async () => {});
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test.skip('exemplo', async () => {
+    const response = await supertest(app.server)
+      .get('/places/autocomplete')
+      .query({
+        query: 'pizza em l',
+      });
+
+    expect(response.status).toBe(200);
+    console.log(response.body);
+  });
+
+  test('caso 1: sucesso (2XX)', async () => {
+    // Implemente aqui...
+  });
+
+  test('caso 2: erro (4XX ou 5XX)', async () => {
+    // Implemente aqui...
+  });
+});
